0a55536508601881189c0ee4a6d89a43fd32ca03
Guilherme Ferreira <guifs2@hotmail.com>
2020-04-23 19:42:19 -0300

    Added attempt output

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ random_value = randint(1, 50)
 print(random_value)
 
 user_name = input("Hey there, my name is Boot, what is yours? ")
-print(user_name + " this is how it's gonna work:")
+print(user_name, " this is how it's gonna work:")
 print("You will have 5 chances to guess the secret number that I am thinking of in a range between 1 and 50, got it?")
 print("Let's game \n")
 
@@ -12,21 +12,21 @@ user_guess = int(input("So wich number do you have in mind? "))
 
 for i in range(4):
     if (user_guess == random_value):
-        print(user_name+" Congrats, you've won!")
+        print(user_name," Congrats, you've won!")
         break
     elif (user_guess > random_value):
 
-        print(user_name+" Sorry, I though of a number a little bit smaller")
+        print("Sorry, I though of a number a little bit smaller")
         if (i <= 4):
-            user_guess = int(input("Okay, try again "))
+            print("Attempts left: ", i)
+            user_guess = int(input("Try again "))
         else:
             break
 
     else:
-        print(user_name+" Sorry, I though of a number a little bit bigger")
+        print("Sorry, I though of a number a little bit bigger")
         if (i <= 4):
-            user_guess = int(input("Okay, try again "))
+            print("Attempts left: ", i)
+            user_guess = int(input("Try again "))
         else:
             break
-
-#print("Attempts left: " + i)
