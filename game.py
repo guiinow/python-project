af4544b0f908e40c2c5c7917738a1e818ed87f47
Guilherme Ferreira <guifs2@hotmail.com>
2020-04-23 19:18:00 -0300

    Wellcome message

---
+++ game.py
@@ -0,0 +1,12 @@
+import random as randint
+
+random_value = randint(1, 50)
+
+user_name = input("Hey there, my name is Boot, what is yours?")
+print(user_name+" this is how it's gonna work:\n")
+
+print("You will have 5 chances to guess the secret number that I am thinking in a range between 1 and 50, got it\n")
+
+print("Are you ready for the game?\n")
+
+#user_guess = int(input("Hey there, are you ready for the game?"))
